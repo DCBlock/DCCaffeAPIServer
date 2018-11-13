70232d5c10b3803f4e97289afbf7c09490113fd6
shseo@digicaps.com <shseo@digicaps.com>
2018-11-13 09:33:26 +0900

    project initalize

---
+++ src/main/java/com/digicap/dcblock/dcblockapiserver/DcblockApiServerApplication.java
@@ -0,0 +1,12 @@
+package com.digicap.dcblock.dcblockapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcblockApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcblockApiServerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/digicap/dcblock/dcblockapiserver/service/HelloService.java
@@ -0,0 +1,8 @@
+package com.digicap.dcblock.dcblockapiserver.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+}
