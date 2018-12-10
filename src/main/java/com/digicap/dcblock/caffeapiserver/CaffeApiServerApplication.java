39f4e69322ac09f73f69476aeb8aad5388f9e191
shseo@digicaps.com <shseo@digicaps.com>
2018-12-10 10:36:15 +0900

    comment Resource Server

--- src/main/java/com/digicap/dcblock/caffeapiserver/CaffeApiServerApplication.java
+++ src/main/java/com/digicap/dcblock/caffeapiserver/CaffeApiServerApplication.java
@@ -4,14 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableScheduling
-@EnableWebSecurity // for JWT
-@EnableGlobalMethodSecurity(prePostEnabled = true) // for JWT
+//@EnableWebSecurity // for JWT
+//@EnableGlobalMethodSecurity(prePostEnabled = true) // for JWT
 public class CaffeApiServerApplication {
 
 	public static void main(String[] args) {
