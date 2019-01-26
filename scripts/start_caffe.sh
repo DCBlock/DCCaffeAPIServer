0bf10e7e497a662e3167312b7881a3c72acf4b8f
shseo@digicaps.com <shseo@digicaps.com>
2019-01-26 15:38:12 +0900

    [UPDATE]구동 스크립트에 pinpoint 설정 추가

--- scripts/start_caffe.sh
+++ scripts/start_caffe.sh
@@ -1,11 +1,14 @@
-# No Redirection
-#JAVA_HOME=/root/DcBlock/install/java-se-8u40-ri
-#nohup $JAVA_HOME/bin/java -jar /root/DcBlock/install/CaffeAPIServer-0.0.1.jar --spring.config.location=file:/root/DcBlock/application.yml 1>/dev/null 2>&1 &
-
-# stdout, stderr
+# JAVA
 JAVA_HOME=/usr/bin
-APP_PATH=/opt/dccaffe/CaffeAPIServer-0.0.2.jar
-CONFIG_PATH=/opt/dccaffe/application.yml
 
-nohup $JAVA_HOME/java -jar -Djava.net.preferIPv4Stack=true $APP_PATH --spring.config.location=$CONFIG_PATH 1>/var/log/dccaffe/stdout.log 2>/var/log/dccaffe/stderr.log &
-#nohup $JAVA_HOME/java -jar -Djava.net.preferIPv4Stack=true /root/dcblock/install/CaffeAPIServer-0.0.1.jar 1>/root/dcblock/logs/stdout.log 2>/root/dcblock/logs/stderr.log &
+# APP
+APP_PATH=/opt/dccaffe/DCCaffeAPI-0.0.2.jar
+CONFIG_PATH=/opt/dccaffe/application.properties
+
+# pinpoint
+PINPOINT=/opt/pinpoint-agent/pinpoint-bootstrap-1.8.1.jar
+AGENT_ID=3
+APPLICATION_NAME=DCCaffeAPI
+
+nohup $JAVA_HOME/java -javaagent:$PINPOINT -Dpinpoint.agentId=$AGENT_ID -Dpinpoint.applicationName=$APPLICATION_NAME  -jar -Djava.net.preferIPv4Stack=true $APP_PATH --spring.config.location=$CONFIG_PATH 1>/var/log/dccaffe/stdout.log 2>/var/log/dccaffe/stderr.log &
+#nohup $JAVA_HOME/java -javaagent:$PINPOINT -Dpinpoint.agentId=$AGENT_ID -Dpinpoint.applicationName=$APPLICATION_NAME  -jar -Djava.net.preferIPv4Stack=true /root/dcblock/install/CaffeAPIServer-0.0.1.jar 1>/root/dcblock/logs/stdout.log 2>/root/dcblock/logs/stderr.log &
